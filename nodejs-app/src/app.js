593d02d72d257b4111ef973478db8c02dd5b2c72
princewadhwa <princewadhwa50@yahoo.com>
2025-09-07 18:06:10 +0530

    Add changes for Trivy stage

--- nodejs-app/src/app.js
+++ nodejs-app/src/app.js
@@ -4,7 +4,38 @@ const app = express();
 const port = process.env.PORT || 3000;
 const color = process.env.DEPLOY_COLOR || "blue";
 
-app.get('/', (req, res) => res.send(`<h1>Hello from Node.js app - ${color} version deployed on Minikube K8s cluster!</h1>`));
+app.get('/', (req, res) => {
+  const style = `
+    <style>
+      body {
+        background-color: ${color};
+        color: white;
+        font-family: Arial, sans-serif;
+        display: flex;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+        margin: 0;
+      }
+      h1 {
+        font-size: 3rem;
+      }
+    </style>
+  `;
+
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Node.js App</title>
+        ${style}
+      </head>
+      <body>
+        <h1>Hello from Node.js app - ${color} version deployed on Minikube K8s cluster!</h1>
+      </body>
+    </html>
+  `);
+});
 
 // Export the app for testing
 module.exports = app;
--- nodejs-app/src/test/sample.test.js
+++ nodejs-app/src/test/sample.test.js
@@ -2,10 +2,15 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('GET /', () => {
-  it('respond with Hello World', (done) => {
+  it('responds with expected heading containing deployed color', (done) => {
     request(app)
       .get('/')
       .expect(200)
-      .expect('<h1>Hello from Node.js app - blue version deployed on Minikube K8s cluster!</h1>', done);
+      .expect(res => {
+        if (!res.text.includes('<h1>Hello from Node.js app - blue version deployed on Minikube K8s cluster!</h1>')) {
+          throw new Error('Response does not contain expected heading');
+        }
+      })
+      .end(done);
   });
 });
